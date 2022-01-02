483c6f26b8503b167a1d13792836040cb84734ce
Aleksei Allen <aqml@mail.ru>
2022-01-02 10:56:09 +0300

    Add project files.

---
+++ WpfHW19/Models/CircleLength.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfHW19.Models
+{
+    static class CircleLength
+    {
+        public static double Calculate(double a) => Math.Round((2 * Math.PI * a), 2);
+    }
+}
